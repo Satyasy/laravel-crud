395ae4a2ce47cf668b4b520dd60dc12db276e0fa
Satyasy <satyavpandega@gmail.com>
2025-02-17 06:34:01 +0000

    Revisioning beforeInstall

--- Scripts/beforeInstall.sh
+++ Scripts/beforeInstall.sh
@@ -2,6 +2,6 @@
 apt update -y 
 systemctl stop apache2
 systemctl disable apache2
-apt install nginx php php-fpm php-xml php-cli php-curl php-mbstring php-mysqlnd composer git -y
+apt install -y nginx php php-fpm php-xml php-cli php-curl php-mbstring php-mysqlnd composer git
 systemctl start nginx
 systemctl enable nginx
--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cd /var/www/filament
+php artisan serv 
